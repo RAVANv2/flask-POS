fbdd08528c6dfe58a8be6437008de14437c99aee
RAVANv2 <chauhanharsh3363334#gmail.com>
2020-07-31 12:25:47 +0530

    read_use

--- pos_tag.py
+++ pos_tag.py
@@ -87,5 +87,3 @@ class pos(my_dictionary):
 
 # Initialize the class object 
 pos_obj = pos()
-text = '''Elon Musk has shared a photo of the spacesuit designed by SpaceX. This is the second image shared of the new design and the first to feature the spacesuit’s full-body look.'''
-print(pos_obj.result(text))
